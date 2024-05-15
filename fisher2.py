069d5dea6cd59b199de81fec435607faa55e0f3f
Eric on Pawsey <ejong@garrawarla-2.pawsey.org.au>
2024-05-15 11:00:34 +0800

    Added ability to read settings for yaml file

--- fisher2.py
+++ fisher2.py
@@ -9,6 +9,48 @@ import os
 import time
 import datetime
 
+def get_config(filename):
+    with open(filename) as f:
+        temp = yaml.safe_load(f)
+
+        if ("ra" in temp.keys()):
+            ra = temp["ra"]
+        else:
+            sys.exit("Please include ra in config file")
+
+        if ("dec" in temp.keys()):
+            dec = temp["dec"]
+        else:
+            sys.exit("Please include dec in config file")
+
+        if ("noise" in temp.keys()):
+            noise = float(temp["noise"])
+        else:
+            sys.exit("Please include noise in config file")
+
+        if ("lambda" in temp.keys()):
+            lamb = temp["lambda"]
+        else:
+            sys.exit("Please include lambda in config file")
+
+        if ("D" in temp.keys()):
+            D = temp["D"]
+        else:
+            sys.exit("Please include D in config file")
+
+        if ("srclist" in temp.keys()):
+            srclist = temp["srclist"]
+        else:
+            sys.exit("Please include srclist in config file")
+
+        if ("metafits" in temp.keys()):
+            metafits = temp["metafits"]
+        else:
+            sys.exit("Please include metafits in config file")
+
+    return ra, dec, noise, lamb, D, srclist, metafits
+
+
 def print_with_time(string):
     time_str = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     print(f'[{time_str}] ' + string)
@@ -198,27 +240,33 @@ def calculate_fim(source_list, metafits_dir, sigma):
         plt.matshow(abs(fim_cos))
         plt.colorbar()
         plt.title(obs + " FIM")
-        plt.savefig("fim_cos.pdf")
+        plt.savefig("fim_cos.pdf", bbox_inches = "tight")
         plt.clf()
         
         plt.matshow(abs(crb))
         plt.colorbar()
         plt.title(obs + " CRB")
-        plt.savefig("crb.pdf")
+        plt.savefig("crb.pdf", bbox_inches = "tight")
         plt.clf()
 
 
 # TODO: IMPLEMENT RADIOMETER EQUATION
 if __name__ == '__main__':
 
-    ra_ph = 72.5
-    dec_ph = -13.35
-    sigma = 300e-3
+    if (len(sys.argv) < 2):
+        sys.exit("Please provide name of the config yaml file")
 
-    srclist_dir = '/scratch/mwaeor/ejong/srclist/srclist_pumav3_EoR0LoBES_EoR1pietro_CenA-GP_2023-11-07.yaml'
+    config = sys.argv[1]
+    ra_ph, dec_ph, sigma, lamb, D, srclist_dir, metafits_dir = get_config(config)
+    # ra_ph = 72.5
+    # dec_ph = -13.35
+    # sigma = 300e-3
+
+    #srclist_dir = '/scratch/mwaeor/ejong/srclist/srclist_pumav3_EoR0LoBES_EoR1pietro_CenA-GP_2023-11-07.yaml'
     # srclist_dir = 'test.yaml'
-    metafits_dir = '/scratch/mwaeor/ejong/SKAEOR15_145_data/rerun_1/solutions/'
+    # metafits_dir = '/scratch/mwaeor/ejong/SKAEOR15_145_data/rerun_1/solutions/'
 
+    print_with_time(f'INPUT SETTINGS: ra={ra_ph} dec={dec_ph} noise={sigma} lambda={lamb} D={D}')
     # Get observations
     get_obs_vec(metafits_dir)
 
