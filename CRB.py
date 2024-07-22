Fixed the angular power spectra, added covariance terms, realised the errors before binning

CRB.py
@@ -403,7 +403,8 @@ def calculate_fim(
             f.write(" ".join([str(abs(a)) for a in row]) + "\n")
 
     # Calculate the CRB, which is the inverse of the FIM
-    crb = np.sqrt(np.linalg.inv(fim_cos))
+    # crb = np.sqrt(np.linalg.inv(fim_cos))
+    crb = np.linalg.inv(fim_cos)
 
     with open(output + "/" + "crb_abs.txt", "w") as f:
         for row in crb:
@@ -413,11 +414,9 @@ def calculate_fim(
         for row in crb:
             f.write(" ".join([str(a) for a in row]) + "\n")
 
-    diag = np.diagonal(abs(crb))
-
-    with open(output + "/" + "diag.txt", "w") as f:
-        for row in diag:
-            f.write(str(row) + "\n")
+        # with open(output + "/" + "diag.txt", "w") as f:
+        #     for row in diag:
+        #         f.write(str(row) + "\n")
 
         # plt.matshow(abs(fim_cos))
         # plt.colorbar()
@@ -434,7 +433,7 @@ def calculate_fim(
         # plt.plot(range(0, 128), diag)
         # plt.savefig(output + "/" + "diag.pdf", bbox_inches="tight")
 
-        return diag
+        return crb
 
 
 @jit(nopython=True, cache=True)

main.py
@@ -1,3 +1,4 @@
+import random
 import sys
 import time
 from pathlib import Path
@@ -18,7 +19,7 @@ from misc import print_with_time
 
 def get_config(filename):
     with open(filename) as f:
-        temp = yaml.safe_load(f)
+        temp = yaml.load(f, Loader=Loader)
 
         if "ra" in temp.keys():
             ra = temp["ra"]
@@ -153,6 +154,7 @@ def save_hdf5(
 # Assuming the phase centre is at zenith
 def main():
     np.set_printoptions(linewidth=np.inf)
+    random.seed(0)
 
     if len(sys.argv) < 2:
         sys.exit("Please provide name of the config yaml file")
@@ -224,6 +226,7 @@ def main():
     freq_array = np.linspace(start_freq, end_freq, num_freq)
 
     pows = list()
+    start_time = time.time()
     # Loop over frequencies and calculate the visibility uncertainties for each frequency
     for i in range(0, len(freq_array)):
         freq = freq_array[i]
@@ -244,29 +247,37 @@ def main():
         sorted_source_list = CRB.attenuate(sorted_fov, beam, l_arr, m_arr, output)
 
         # Calculate FIM
-        print_with_time("CALCULATING THE FIM")
-        uncertainties = CRB.calculate_fim(
+        print_with_time("CALCULATING THE CRB")
+        covar_mat = CRB.calculate_fim(
             sorted_source_list, baseline_lengths, num_ant, lamb, freq, sigma, output
         )
 
-        mean_CRB = np.mean(uncertainties)
-        print_with_time(f"MEAN CRB: {mean_CRB}")
+        # mean_CRB = np.mean(uncertainties)
+        # print_with_time(f"MEAN CRB: {mean_CRB}")
 
         # Propagate errors into visibilities
         print_with_time("PROPAGATING INTO VISIBILITIES")
         vis_uncertainties = propagate.propagate(
-            baseline_lengths, sorted_source_list, uncertainties, lamb
+            baseline_lengths, sorted_source_list, covar_mat, lamb
         )
+
+        print_with_time("REALIZE ERRORS")
+        realised_unc = propagate.realise(vis_uncertainties)
 
         print_with_time("BINNING ERRORS")
         vis_mat, u_arr, v_arr = power.uv_bin(
-            lamb, vis_uncertainties, baseline_lengths, output
+            lamb, realised_unc, baseline_lengths, output
         )
 
         # Calculate average power in annuli for this particular frequency
-        print_with_time("POWER SPECTRUM")
+        print_with_time("ANGULAR POWER SPECTRUM")
         pow = power.power_bin(vis_mat, u_arr, v_arr, k_perp, output)
         pows.append(pow)
+
+        # plt.plot(k_perp, pow)
+        # plt.xlabel("k_perp")
+        # plt.ylabel("Average visibility rms")
+        # plt.show()
 
     pows = np.array(pows)
     pows_fft = np.fft.fft(pows, axis=0)
@@ -279,6 +290,9 @@ def main():
     folded_pow = pows_fft[0:mid, :]
     folded_pow[1:, :] = (folded_pow[1:, :] + np.flip(pows_fft[mid:, :], axis=0)) / 2.0
     # folded[1:] = folded[1:] + np.flip(freq_array_fft[mid:])
+
+    end_time = time.time()
+    print_with_time(f"TOTAL RUN TIME: {end_time - start_time}")
 
     save_hdf5(
         k_perp,

power.py
@@ -140,6 +140,8 @@ def power_bin(vis_mat, u_arr, v_arr, k_perp, output):
 
     pow = np.zeros(len(k_perp), dtype=np.complex64)
 
+    # Loop through all grid points, check if their position are within the annuli
+    # and if the grid has a non-zero value
     for i in range(1, len(k_perp)):
         num_in_annuli = 0
         for j in range(0, len(u_arr)):
@@ -147,11 +149,15 @@ def power_bin(vis_mat, u_arr, v_arr, k_perp, output):
             for k in range(0, len(v_arr)):
                 v = v_arr[k]
                 dist = np.sqrt(u**2 + v**2)
+                if np.abs(vis_mat[j, k]) == 0:
+                    continue
+
                 if dist >= k_perp[i - 1] and dist < k_perp[i]:
                     num_in_annuli += 1
                     pow[i] += vis_mat[j, k]
 
-        pow[i] /= num_in_annuli
+        if num_in_annuli != 0:
+            pow[i] /= num_in_annuli
 
     pow *= np.conjugate(pow)
 

propagate.py
@@ -1,7 +1,36 @@
 import numpy as np
 from numba import jit, prange
 
-from misc import print_with_time
+
+@jit(nopython=True, cache=True, parallel=True)
+def realise(vis_uncertainties):
+    """Function to realize the visibility errors by sampling 0-mean vis
+
+    Parameters
+    ----------
+    - vis_uncertainties: `array`
+        2D array of visibility errors
+
+    Returns
+    -------
+    - realised_unc: `array`
+    """
+
+    num_ant = vis_uncertainties.shape[0]
+
+    realised_unc = np.zeros((num_ant, num_ant), dtype=np.complex64)
+    for a in prange(0, num_ant):
+        for b in range(0, num_ant):
+            if vis_uncertainties[a, b].imag > 10 ^ -30:
+                print(a, b, vis_uncertainties[a, b].imag)
+                exit("IMAGINARY PART QUITE LARGE")
+            sigma = vis_uncertainties[a, b].real / np.sqrt(2)
+            real = np.random.normal(0, sigma)
+            imag = np.random.normal(0, sigma)
+
+            realised_unc[a, b] = real + 1j * imag
+
+    return realised_unc
 
 
 @jit(nopython=True, cache=True)
@@ -34,9 +63,8 @@ def partial(u, v, gain, source_list):
     return result * gain
 
 
-@jit(nopython=True, cache=True, parallel=True)
-def partial_star(u, v, source_list, gain):
-    """Function for evaluating the complex conjugate partial derivative expression
+def partial_star(u, v, gain, source_list):
+    """Function for evaluating the partial derivative expression
 
     Parameters
     ----------
@@ -54,6 +82,7 @@ def partial_star(u, v, source_list, gain):
     - result: `complex`
         result of the partial derivative expression
     """
+
     result = 0
     for i in prange(0, len(source_list)):
         result += source_list[i, 2] * np.exp(
@@ -63,18 +92,18 @@ def partial_star(u, v, source_list, gain):
     return result * gain
 
 
-@jit(nopython=True, cache=True, parallel=True)
-def propagate(baseline_lengths, source_list, uncertainties, lamb):
+# @jit(nopython=True, cache=True, parallel=True)
+def propagate(baseline_lengths, source_list, covar_mat, lamb):
     """Function for propagating gain errors into visibilities
 
     Parameters
     ----------
     - baselines: `array`
         array of baselines
-    - uncertainties: `array`
-        uncertainties in gains for each antenna
     - source_list: `array`
         source coordinates and intensity (l, m, B)
+    - covar_mat: `array`
+        should be the whole calculated CRB matrix (not square-rooted).
 
     Returns
     -------
@@ -85,22 +114,30 @@ def propagate(baseline_lengths, source_list, uncertainties, lamb):
     # Propagate for a single visibility, i.e. a single baseline
 
     num_ant = baseline_lengths.shape[0]
-    vis_uncertainties = np.zeros((num_ant, num_ant), dtype="float")
+    vis_uncertainties = np.zeros((num_ant, num_ant), dtype=np.complex64)
 
     baselines = baseline_lengths / lamb
+    # Loop through antennas
     for a in prange(0, num_ant):
-        g1_unc = uncertainties[a]
+        g1_var = covar_mat[a, a]
         g1 = 1
         for b in range(0, num_ant):
-            g2_unc = uncertainties[b]
+            g2_var = covar_mat[b, b]
+
+            g1g2_covar = covar_mat[a, b]
+            g2g1_covar = covar_mat[b, a]
+
             g2 = 1
             u = baselines[a, b, 0]
             v = baselines[a, b, 1]
 
             dvdg1 = partial(u, v, g1, source_list)
             dvdg2 = partial(u, v, g2, source_list)
-            vis_uncertainties[a, b] = (
-                np.abs(dvdg1) ** 2 * g1_unc**2 + np.abs(dvdg2) ** 2 * g2_unc**2
-            )
+            dvdg1_star = partial_star(u, v, g1, source_list)
+            dvdg2_star = partial_star(u, v, g2, source_list)
+            vis_uncertainties[a, b] = np.abs(dvdg1) ** 2 * g1_var**2
+            +np.abs(dvdg2) ** 2 * g2_var**2
+            +dvdg1_star * dvdg2 * g2g1_covar
+            +dvdg2_star * dvdg1 * g1g2_covar
 
     return np.sqrt(vis_uncertainties)